89be24daaae34d895004c757e411c8ab38adae48
ladhami <louai.adhami@gmail.com>
2020-05-19 19:44:57 -0400

    2

--- wanlking_figure/Ben_Giangrasso.js
+++ wanlking_figure/Ben_Giangrasso.js
@@ -215,55 +215,62 @@ function colorCube()
 
 window.onload = function init() {
 
-    canvas = document.getElementById( "gl-canvas" );
+    var isAutomated = navigator.webdriver
+    if(isAutomated) {
+        document.getElementById("container").style.display = "none";             
+    } else {            
+        document.getElementById("preview").style.display = "none"; 
+    
+        canvas = document.getElementById( "gl-canvas" );
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+        gl = WebGLUtils.setupWebGL( canvas );
+        if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
+        gl.viewport( 0, 0, canvas.width, canvas.height );
 
-    aspect =  canvas.width/canvas.height;
+        aspect =  canvas.width/canvas.height;
 
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+        gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 
-    gl.enable(gl.DEPTH_TEST);
+        gl.enable(gl.DEPTH_TEST);
 
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+        //
+        //  Load shaders and initialize attribute buffers
+        //
+        var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+        gl.useProgram( program );
 
-    colorCube();
+        colorCube();
 
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
+        var cBuffer = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
 
-    var vColor = gl.getAttribLocation( program, "vColor" );
-    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vColor);
+        var vColor = gl.getAttribLocation( program, "vColor" );
+        gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( vColor);
 
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
+        var vBuffer = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
 
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+        var vPosition = gl.getAttribLocation( program, "vPosition" );
+        gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( vPosition );
 
-    modelView = gl.getUniformLocation( program, "modelView" );
-    projection = gl.getUniformLocation( program, "projection" );
+        modelView = gl.getUniformLocation( program, "modelView" );
+        projection = gl.getUniformLocation( program, "projection" );
 
-// buttons for viewing parameters
+    // buttons for viewing parameters
 
-    document.getElementById("Button1").onclick = function(){theta += (dr * 2);};
-    document.getElementById("Button2").onclick = function(){theta -= (dr * 2);};
-    document.getElementById("Button3").onclick = function(){phi += dr;};
-    document.getElementById("Button4").onclick = function(){phi -= dr;};
-    document.getElementById("Button5").onclick = function(){location.reload();};
-    render();
+        document.getElementById("Button1").onclick = function(){theta += (dr * 2);};
+        document.getElementById("Button2").onclick = function(){theta -= (dr * 2);};
+        document.getElementById("Button3").onclick = function(){phi += dr;};
+        document.getElementById("Button4").onclick = function(){phi -= dr;};
+        document.getElementById("Button5").onclick = function(){location.reload();};
+        render();
+    }
 }
 
 
